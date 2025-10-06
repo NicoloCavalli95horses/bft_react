2c1a2226939f9873dfdc725ebb0edd41dbfe2395
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2025-10-06 16:46:22 +0200

    testing library to disable dev tool

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import App from './App';
 
-const gH = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-const isReact = typeof gH === "object";
-if (!isReact) {}
-
-for (let [key, value] of Object.entries(gH)) {
-  gH[key] = (typeof value === "function")
-    ? () => {}
-    : null;
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
 }
+// const gH = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+// const isReact = typeof gH === "object";
+// if (!isReact) {}
+
+// for (let [key, value] of Object.entries(gH)) {
+//   gH[key] = (typeof value === "function")
+//     ? () => {}
+//     : null;
+// }
 
 
 createRoot(document.getElementById('root')).render(
